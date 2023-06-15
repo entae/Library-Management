1cdd4dc2fb65a99c2f805245c6970c3be3cc72da
Intae Chung <itchung@myseneca.ca>
2023-06-15 13:21:57 -0400

    ws5 lab

--- ws5/lab/Account.cpp
+++ ws5/lab/Account.cpp
@@ -11,15 +11,17 @@
 
 using namespace std;
 namespace sdds {
+
    void Account::setEmpty() {
       m_number = -1;
       m_balance = 0.0;
    }
-   // New account
+
    Account::Account() {  
       m_number = 0;
       m_balance = 0.0;
    }
+
    Account::Account(int number, double balance ) {
       setEmpty();
       if (number >= 10000 && number <= 99999
@@ -49,45 +51,35 @@ namespace sdds {
       }
       return cout;
    }
-//public:
-/*
-The Account class can be in three different states:
-Invalid Empty State
-   An account is in an invalid state if invalid information is fed into the account. 
-   In these types of situations the account number is set to -1 and the balance is set to 0. 
-   The Account object in this case is rendered inactive and can not be used anymore.
-New
-   An account is considered New or not set when it is just created and the account number is not assigned yet. 
-   This state of the Account class is flagged by setting the account number (m_number) to 0.
-Valid
-   An Account is considered valid if the account number is a 5 digit integer with a zero or positive balance.
-*/
 
-//returns true if the account is valid and otherwise false. 
+   //returns true if the account is valid and otherwise false. 
    Account::operator bool()const {
       return m_number >= 10000 && m_number <= 99999 
          && m_balance >= 0;
    }
-//returns the account number.  
+   //returns the account number.  
    Account::operator int()const {
       return m_number;
    }
-//returns the balance value
+   //returns the balance value
    Account::operator double()const {
       return m_balance;
    }
-//returns true is the account is new or not set 
-//(i.e. if the account number is zero), 
-//otherwise it will return false.
+   //returns true is the account is new or not set 
+   //(i.e. if the account number is zero), 
+   //otherwise it will return false.
    bool Account::operator ~()const{
       return m_number == 0;
    }
 
    Account& Account::operator=(int number) {
+      //if instance is not new, nothing happens
       if (operator~()) {
+         //value of number is copied if within range(valid)
          if (number >= 10000 && number <= 99999) {
             m_number = number;
          } else {
+            //if not within range, set to invalid state
             setEmpty();
          }
       }
@@ -95,6 +87,8 @@ Valid
    }
 
    Account& Account::operator=(Account& src) {
+      //if instance is new && src is valid
+      //copies src values into *this and src is set to zero
       if (operator~() && (bool)src) {
          m_number = src.m_number;
          m_balance = src.m_balance;
@@ -105,21 +99,30 @@ Valid
    }
 
    Account& Account::operator+=(double value) {
+      //if *this is valid and value is a non-negative value
       if (*this && value >= 0) {
+         //value is added to the value of m_balance
          m_balance += value;
       }
       return *this;
    }
 
    Account& Account::operator-=(double value) {
+      //if *this is valid and value is positive
+      //and subtraction won't end in a negative value
       if (*this && value > 0 && m_balance >= value) {
+         //value is subtracted from m_balance
          m_balance -= value;
       }
       return *this;
    }
 
    Account& Account::operator<<(Account& src) {
+      //if this and &src are not the same instance
+      // && src and &src are both valid
       if(this != &src && (bool)src && *this) {
+         //argument balance is added to instance balance
+         //and argument balance is set to zero
          m_balance += src.m_balance;
          src.m_balance = 0.0;
       }
@@ -127,16 +130,22 @@ Valid
    }
 
    Account& Account::operator>>(Account& src) {
+      //the position of src and instance object are switched from <<
       src << *this;
       return *this;
    }
 
-  //Binary helper operators
+//Binary helper operators:
+
+   //
    double operator+(const Account& A,const Account& B) {
       double sum = 0.0;
+      //if instances A and B are both valid
       if ((bool)A && (bool)B) {
+         //double value is the sum of A and B's balances
          sum = (double)A + (double)B;
       }
+      //if either instance is invalid 0.0 is returned
       return sum;
    }
 
--- ws5/lab/Account.h
+++ ws5/lab/Account.h
@@ -13,32 +13,21 @@ namespace sdds {
    class Account {
       int m_number;
       double m_balance; 
+
       void setEmpty();
    public:
-   //Account constructors (implemented):
       Account();
       Account(int number, double balance);
       std::ostream& display()const;
-//*To be implemented:
-    //returns true if the account is valid and otherwise false. 
-      //This operator can not modify the account object.
       operator bool() const;
-    //returns the account number.  
       operator int() const;
-    //returns the balance value
       operator double() const;
-    //returns true is the account is new or not set 
-      //(i.e. if the account number is zero), 
-      //otherwise it will return false.
       bool operator ~()const;
-//Binary member operators
-    //Note: All the binary member operators will not take any action 
-    //if the account is in an invalid state.
-      Account& operator=(int num);
+
+      Account& operator=(int number);
       Account& operator=(Account&);
       Account& operator+=(double balance);
       Account& operator-=(double value);
-
       Account& operator<<(Account& src);
       Account& operator>>(Account& src);
 
