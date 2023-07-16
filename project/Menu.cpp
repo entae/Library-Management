221643adfcbee91f8bdf3e2440913cd1c4f9ff92
Intae Chung <itchung@myseneca.ca>
2023-07-15 23:01:14 -0400

    ms1

--- project/Menu.cpp
+++ project/Menu.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include "Menu.h"
+#include "Utils.h"
 
 using namespace std;
 namespace sdds {
@@ -16,8 +17,8 @@ namespace sdds {
 //MenuItem Class methods:
     MenuItem::MenuItem(const char* value) {
         if (value) {
-            m_item = new char [strlen(value) + 1];
-            strcpy(m_item, value);
+            m_item = new char [strLen(value) + 1];
+            strCpy(m_item, value);
         } else {
             m_item = nullptr;
         }
@@ -50,8 +51,8 @@ namespace sdds {
 
     Menu::Menu(const char* title) {
         if (title) {
-            m_title = new char[strlen(title +1)];
-            strcpy(m_title, title);
+            m_title = new char[strLen(title +1)];
+            strCpy(m_title, title);
         } 
         m_numItems = 0;
 		for (unsigned int i = 0; i < MAX_MENU_ITEMS; ++i)
